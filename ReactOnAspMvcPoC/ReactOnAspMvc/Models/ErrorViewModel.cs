a7f7b27111fc31725d93dfd950105969fc3e8a52
antonio.selvi <antonio.selvi@colba.es>
2018-07-04 17:03:01 +0200

    Full project

---
+++ ReactOnAspMvcPoC/ReactOnAspMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReactOnAspMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
